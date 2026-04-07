2e4566b008f62e5aa631b82caafdd9f713457c55
willyO-o <marcggmarc.20@gmail.com>
2026-04-07 01:05:06 -0400

    filtro version 1.0.5

--- config/app.php
+++ config/app.php
@@ -127,7 +127,7 @@ return [
     'aplicacion' => [
         'nombre' => 'Sistema de Mandamientos Judiciales',
         'sigla' => 'SMJ-DACI',
-        'version' => '1.0.4',
+        'version' => '1.0.5',
         'empresa' => 'Felcc',
         'autor' => 'Willy Chana',
     ],
