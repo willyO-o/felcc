ea38e6a6b90f8f550dd9afaf4aaa58aa38141965
willyO-o <marcggmarc.20@gmail.com>
2026-04-15 01:36:08 -0400

    actualizando v 1.0.8

--- config/app.php
+++ config/app.php
@@ -127,7 +127,7 @@ return [
     'aplicacion' => [
         'nombre' => 'Sistema de Mandamientos Judiciales',
         'sigla' => 'SMJ-DACI',
-        'version' => '1.0.6',
+        'version' => '1.0.8',
         'empresa' => 'Felcc',
         'autor' => 'Willy Chana',
     ],
