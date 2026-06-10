e77412b0feee5002dc3f3ebaabb089cd37dcedad
WillyO-o <marcggmarc.20@gmail.com>
2026-06-10 08:50:13 -0400

    subiendo consulta avanzadas

--- config/app.php
+++ config/app.php
@@ -127,7 +127,7 @@ return [
     'aplicacion' => [
         'nombre' => 'Sistema de Mandamientos Judiciales',
         'sigla' => 'SMJ-DACI',
-        'version' => '1.2.3',
+        'version' => '1.2.4',
         'empresa' => 'Felcc',
         'autor' => 'Willy Chana',
     ],
